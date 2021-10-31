56efa887373506016be29870489a9236c250cec5
Swathi04 <swathianandram@gmail.com>
2021-10-31 14:15:31 -0400

    Homepage and login UI

--- Frontend/web-enterprise-sw-platforms/src/app/app.module.ts
+++ Frontend/web-enterprise-sw-platforms/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { EmployeePageComponent } from './employee-page/employee-page.component';
+import { ManagerPageComponent } from './manager-page/manager-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    EmployeePageComponent,
+    ManagerPageComponent,
+    AdminPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
